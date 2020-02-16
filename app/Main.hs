f5bf030c2e8a5858e3fe791e9878f6684482ab51
Simon Lomax <simax.99.99@gmail.com>
2020-02-16 10:37:41 +0000

    Added reader monad tutorial code

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,40 @@
 module Main where
 
-import Lib
+import           Lib
+import           Control.Monad.Reader           ( Reader
+                                                , ask
+                                                , runReader
+                                                )
+import           Prelude                 hiding ( div )
 
 main :: IO ()
-main = someFunc
+main = putStrLn "what is your email address?" >> getLine >>= \email ->
+    putStrLn . show $ runReader view email
+
+view :: Reader Email Html
+view = page >>= \page' -> return $ div [page']
+
+page :: Reader Email Html
+page = content >>= \content' -> return $ div [topNav, content']
+
+topNav :: Html
+topNav = div [h1 ["OurSite.com"]]
+
+content :: Reader Email Html
+content = ask >>= \email -> right >>= \right' ->
+    return $ div [h1 ["Custom Content for " ++ email], left, right']
+
+left :: Html
+left = div [p ["this is the left side"]]
+
+right :: Reader Email Html
+right = article >>= \article' -> return $ div [article']
+
+article :: Reader Email Html
+article =
+    widget >>= \widget' -> return $ div [p ["this is an article"], widget']
+
+widget :: Reader Email Html
+widget = ask >>= \email -> return
+    $ div [p ["Hey " ++ email ++ ", we've got a great offer for you!"]]
 
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,22 @@
-module Lib
-    ( someFunc
-    ) where
+module Lib where
+
+import           Data.List                      ( intercalate )
+import           Prelude                 hiding ( div )
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
+
+type Html = String
+type Email = String
+
+div :: [Html] -> Html
+div children = "<div>" ++ combine children ++ "</div>"
+
+h1 :: [Html] -> Html
+h1 children = "<h1>" ++ combine children ++ "</h1>"
+
+p :: [Html] -> Html
+p children = "<p>" ++ combine children ++ "</p>"
+
+combine :: [Html] -> Html
+combine = intercalate ""
